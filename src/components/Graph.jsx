569de8a0c25f6e08806fb9acf61678699c2fc685
Alexander <alexander.niehaus@gmx.net>
2025-01-20 20:32:15 +0100

    add Graph component

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const data = [1, 2, 3, 4, 5, 6, 7, 10, 8, 9];
 
 const Graph = () => {
+  const chartData = {
+    labels: data.map((_, idx) => idx + 1),
+    datasets: [
+      {
+        label: "Array Values",
+        data: data,
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
   return (
-    <div className="h-screen flex flex-row justify-center items-center">
-      <p className="text-black">Hallo</p>
+    <div className="top-14">
+      <Bar data={chartData} options={options} />
     </div>
   );
 };
